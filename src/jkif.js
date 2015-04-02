e20d35c5b48c2ee36a9f96977d7505eb15bd0b05
Clark Feusier <cfeusier@gmail.com>
2015-04-01 21:04:29 -0700

    Refactor jkif entry to look sexier

--- src/jkif.js
+++ src/jkif.js
@@ -1,8 +1,7 @@
 var fs = require('fs'),
     Parser = require('jison').Parser,
-    jKif = {},
-    grammars = fs.readFileSync(__dirname + '/grammars.jison', 'utf8');
-
-jKif.Parser = new Parser(grammars);
+    grammars = fs.readFileSync(__dirname + '/grammars.jison', 'utf8'),
+    jKifParser = new Parser(grammars),
+    jKif = { Parser: jKifParser };
 
 module.exports = jKif;
