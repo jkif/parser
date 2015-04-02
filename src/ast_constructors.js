bb08813583a84a31a71f3d7bec90903f6e6bdfcd
Clark Feusier <cfeusier@gmail.com>
2015-04-01 19:22:20 -0700

    Add VariableNode constructor to ast for Row and Ind variables

--- src/ast_constructors.js
+++ src/ast_constructors.js
@@ -4,6 +4,19 @@ function KIFNode(kifExpressions) {
   this.expressions = this.expressions.concat(kifExpressions);
 }
 
+function WordNode(identifier) {
+  this.type = 'WordNode';
+  this.word = identifier;
+}
+
+function VariableNode(identifier, variableType) {
+  this.type = 'VariableNode';
+  this.variableType = variableType || 'IND';
+  this.variableName = identifier;
+}
+
 module.exports = {
-  KIFNode: KIFNode
+  KIFNode: KIFNode,
+  WordNode: WordNode,
+  VariableNode: VariableNode
 };
