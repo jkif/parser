75e4128e958709239e3a87787ad9e8cffbd6e851
Clark Feusier <cfeusier@gmail.com>
2015-04-01 18:32:32 -0700

    Add ast_constructors module with base KIFNode constructor

---
+++ src/ast_constructors.js
@@ -0,0 +1,9 @@
+function KIFNode(kifExpressions) {
+  this.type = 'KIFNode';
+  this.expressions = this.expressions || [];
+  this.expressions = this.expressions.concat(kifExpressions);
+}
+
+module.exports = {
+  KIFNode: KIFNode
+};
