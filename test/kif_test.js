3476543efbeea6c0a10c620986d503be7d5fbd5e
Clark Feusier <cfeusier@gmail.com>
2015-04-02 17:26:03 -0700

    Finish Equivalence grammar and test coverage

--- test/kif_test.js
+++ test/kif_test.js
@@ -264,6 +264,14 @@ describe('jKif', function() {
         expect(parsed.antecedent).to.be.an.instanceof(ast.WordNode);
         expect(parsed.consequent).to.be.instanceof(ast.WordNode);
       });
+      it('correctly parses an equivalence into an EquivalenceNode', function() {
+        var parsed = jKif.Parser.parse('(<=> expr1 expr2)').expressions[0];
+        expect(parsed).to.be.an.instanceof(ast.EquivalenceNode);
+        expect(parsed.expressions).to.be.an.instanceof(Array);
+        expect(parsed.expressions).to.have.length(2);
+        expect(parsed.expressions[0]).to.be.an.instanceof(ast.WordNode);
+        expect(parsed.expressions[1]).to.be.an.instanceof(ast.WordNode);
+      });
     });
   });
 });
