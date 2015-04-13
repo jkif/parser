ed6f95ba17c2878e0df9e2485d389c1bdc05e8b5
Clark Feusier <cfeusier@gmail.com>
2015-04-12 21:12:45 -0700

    Add tests for tdding the thinker's isConsistentSentential method

--- test/specs/thinker/api/is_consistent_sentential_test.js
+++ test/specs/thinker/api/is_consistent_sentential_test.js
@@ -6,15 +6,32 @@ var chai = require('chai'),
 
 describe('jKif.Thinker #isConsistentSentential', function() {
 
-  // var parsedEmpty = jKif.Parser.parse('');
-  // var emptyKb = jKif.Utility.knowledgeBase(parsedEmpty);
+  it('should return a Boolean value', function() {
+    var parsedEmpty = jKif.Parser.parse('');
+    var emptyKb = jKif.Utility.knowledgeBase(parsedEmpty);
+    expect(jKif.Thinker.isConsistentSentential(emptyKb)).to.be.a('boolean');
+  });
 
-  // it('should return a Boolean value', function() {
-  //   expect(jKif.Thinker.isConsistentSentential(emptyKb)).to.be.a('boolean');
-  // });
+  it('should return true for single fact knowledgebases', function() {
+    var parsedFact = jKif.Parser.parse('(GovernmentFn UnitedStates)');
+    var simpleKb = jKif.Utility.knowledgeBase(parsedFact);
+    var isCons = jKif.Thinker.isConsistentSentential(simpleKb);
+    expect(isCons).to.be.true;
+  });
 
 
-  // var parsedAtom = jKif.Parser.parse('(GovernmentFn UnitedStates)');
-  // var simpleKb = jKif.Utility.knowledgeBase(parsedAtom);
-  // var tree = jKif.Thinker.isConsistentSentential(simpleKb);
+  it('should return true for multiple fact knowledgebases where the facts are unrelated', function() {
+    var parsedCons = jKif.Parser.parse('(GovernmentFn UnitedStates)(instance ?MOSES Human)');
+    var multKb = jKif.Utility.knowledgeBase(parsedCons);
+    var isCons = jKif.Thinker.isConsistentSentential(multKb);
+    expect(isCons).to.be.true;
+  });
+
+  it('should return false for multiple fact knowledgebases where the facts contradict each other', function() {
+    var parsedInCons = jKif.Parser.parse('(not (instance ?MOSES Human))(instance ?MOSES Human)');
+    var multKb = jKif.Utility.knowledgeBase(parsedInCons);
+    var isInCons = jKif.Thinker.isConsistentSentential(multKb);
+    expect(isInCons).to.be.false;
+  });
+
 });
