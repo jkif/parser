d78ec99fa20b3a47932a0046b2c71f086b85bce8
Clark Feusier <cfeusier@gmail.com>
2015-04-13 22:44:31 -0700

    Convert tests to pending for constructTree enhancement

--- test/specs/thinker/truth_trees/construct_tree_test.js
+++ test/specs/thinker/truth_trees/construct_tree_test.js
@@ -13,25 +13,25 @@ describe('#constructTree helper method', function() {
       constructTreeReturn = ttSent.tree; // constructTree sets ttSent.tree
 
 
-  it('should return an instance of an Object', function() {
+  xit('should return an instance of an Object', function() {
     expect(constructTreeReturn).to.be.an.instanceof(Object);
   });
 
-  it('should return an Object with trunk, paths, step count, and prop map', function() {
+  xit('should return an Object with trunk, paths, step count, and prop map', function() {
     expect(constructTreeReturn).to.have.all.keys(['trunk', 'paths', 'stepCount', 'propMap']);
   });
 
-  it('should return a tree with a stepCount of 0 for empty knowledgebase inputs', function() {
+  xit('should return a tree with a stepCount of 0 for empty knowledgebase inputs', function() {
     expect(constructTreeReturn.stepCount).to.equal(0);
   });
 
-  it('should return a tree with a stepCount of 1 for knowledgebase inputs with a single atomic sentence', function() {
+  xit('should return a tree with a stepCount of 1 for knowledgebase inputs with a single atomic sentence', function() {
     var atomicKb = jKif.Utility.knowledgeBase(jKif.Parser.parse('(instance ?CLARK Human)'));
     var ttSentAtomic = new tt.TruthTreeSent(atomicKb);
     expect(ttSentAtomic.stepCount).to.equal(1);
   });
 
-  it('should return a tree with a stepCount of 2 for knowledgebase inputs with a single negation of an atomic sentence', function() {
+  xit('should return a tree with a stepCount of 2 for knowledgebase inputs with a single negation of an atomic sentence', function() {
     var atomicNegationKb = jKif.Utility.knowledgeBase(jKif.Parser.parse('(not (instance ?CLARK Human))'));
     var ttSentAtomicNegation = new tt.TruthTreeSent(atomicNegationKb);
     expect(ttSentAtomicNegation.stepCount).to.equal(2);
