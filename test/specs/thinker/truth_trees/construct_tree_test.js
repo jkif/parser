00d91ab654f9f0d1724b6fc6ed92c6b2f7f3cbbd
Clark Feusier <cfeusier@gmail.com>
2015-04-20 18:51:31 -0700

    Refactor node decompose method and add simple tests

--- test/specs/thinker/truth_trees/construct_tree_test.js
+++ test/specs/thinker/truth_trees/construct_tree_test.js
@@ -7,14 +7,26 @@ var chai = require('chai'),
 
 describe('#construct helper method', function() {
 
-  var parsed = jKif.Parser.parse(''),
-      kb = jKif.Utility.knowledgeBase(parsed),
+  var kb = jKif.Utility.knowledgeBase(jKif.Parser.parse('')),
       ttSent = new tt.TruthTreeSent(kb),
-      atomicKb = jKif.Utility.knowledgeBase(jKif.Parser.parse('(not (instance ?CLARK Human))(instance ?CLARK Human)')),
+      atomicKb = jKif.Utility.knowledgeBase(jKif.Parser.parse('(instance ?CLARK Human)')),
       ttSentAtomic = new tt.TruthTreeSent(atomicKb);
 
   it('should return an instance of an Object', function() {
     expect(ttSent).to.be.an.instanceof(Object);
+  });
+
+  it('should return an empty open stack for an empty knowledgeBase', function() {
+    expect(ttSent.openStacks[0]).to.exist;
+    expect(ttSent.openStacks[0]).to.be.empty;
+  });
+
+  it('should return an open stack with a single reduced node for a single atomic sentence knowledgeBase', function() {
+    var openStack = ttSentAtomic.openStacks[0];
+    expect(openStack).to.exist;
+    expect(openStack).to.have.length(1);
+    expect(openStack[0].checked).to.be.true;
+    expect(openStack[0]._id).to.be.a('number');
   });
 
 });
