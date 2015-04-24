70fc2b62be4c6937b71338f1503ead0146de85bc
Clark Feusier <cfeusier@gmail.com>
2015-04-23 18:04:28 -0700

    Finish addition of complex negation truth tree specs

--- test/specs/thinker/truth_trees/complex_negations_test.js
+++ test/specs/thinker/truth_trees/complex_negations_test.js
@@ -60,4 +60,50 @@ describe('#construct with complex negations', function() {
     expect(secondStack[2]._id).to.equal(-4);
   });
 
+  xit('should return one open stack with 4 nodes in the stack for a negated implication (atoms)', function() {
+    var negatedImplicationKb = jKif.Utility.knowledgeBase(jKif.Parser.parse('(not (=> (instance ?CLARK Human)(isStupid ?CLARK)))')),
+        ttSentNegatedImplication = new tt.TruthTreeSent(negatedImplicationKb),
+        openStack = ttSentNegatedImplication.openStacks[0];
+    expect(Object.keys(ttSentNegatedImplication.openStacks)).to.have.length(1);
+    expect(openStack).to.exist;
+    expect(openStack).to.have.length(4);
+    expect(openStack[0].checked).to.be.true;
+    expect(openStack[0]._id).to.be.a('string');
+    expect(openStack[0]._id).to.equal('negation');
+    expect(openStack[1].checked).to.be.true;
+    expect(openStack[1]._id).to.be.a('string');
+    expect(openStack[1]._id).to.equal('molecule');
+    expect(openStack[2].checked).to.be.true;
+    expect(openStack[2]._id).to.be.a('number');
+    expect(openStack[2]._id).to.equal(3);
+    expect(openStack[3].checked).to.be.true;
+    expect(openStack[3]._id).to.be.a('number');
+    expect(openStack[3]._id).to.equal(-4);
+  });
+
+  xit('should return two open stacks with four nodes in each stack for a negated equivalence (simple atomic)', function() {
+    var negatedEquivalenceKb = jKif.Utility.knowledgeBase(jKif.Parser.parse('(not (<=> (isAlive ?CLARK)(isUgly ?CLARK)))')),
+        ttSentNegatedEquivalence = new tt.TruthTreeSent(negatedEquivalenceKb),
+        openStacks = ttSentNegatedEquivalence.openStacks;
+    expect(openStacks).to.exist;
+    expect(Object.keys(openStacks)).to.have.length(2);
+    expect(Object.keys(ttSentNegatedEquivalence.closedStacks)).to.be.empty;
+    expect(openStacks[0][0].checked).to.be.true;
+    expect(openStacks[0][0]._id).to.equal('negation');
+    expect(openStacks[0][1].checked).to.be.true;
+    expect(openStacks[0][1]._id).to.equal('molecule');
+    expect(openStacks[0][2].checked).to.be.true;
+    expect(openStacks[0][2]._id).to.equal(3);
+    expect(openStacks[0][3].checked).to.be.true;
+    expect(openStacks[0][3]._id).to.equal(-4);
+    expect(openStacks[1][0].checked).to.be.true;
+    expect(openStacks[1][0]._id).to.equal('negation');
+    expect(openStacks[1][1].checked).to.be.true;
+    expect(openStacks[1][1]._id).to.equal('molecule');
+    expect(openStacks[1][2].checked).to.be.true;
+    expect(openStacks[1][2]._id).to.equal(-3);
+    expect(openStacks[1][3].checked).to.be.true;
+    expect(openStacks[1][3]._id).to.equal(4);
+  });
+
 });
